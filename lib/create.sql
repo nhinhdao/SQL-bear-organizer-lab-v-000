e7d0cb2126c43454c2af652f3296d25f0361edc5
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-07-22 22:51:07 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,1 @@
-create table bears (id INTEGER, name text, age INTEGER, gender text, color text, temperament text, alive INTEGER);
+create table bears (id INTEGER primary key, name text, age INTEGER, gender text, color text, temperament text, alive INTEGER);
