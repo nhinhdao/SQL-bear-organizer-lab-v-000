ff767dad23279cb3dcf2b19adca81d241f599442
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-07-22 23:04:14 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,13 +19,13 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "select count()"
+  "select count(color) from bears where "
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "select count(*) where temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "select * from bears where nam = null"
 end
